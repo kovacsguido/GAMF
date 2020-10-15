103e3356aa4fac650cd84ad4ba2a57c0892d0f01
Tóth László <lastoth@gmail.com>
2020-10-15 11:18:10 +0200

    Add project files.

---
+++ Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GAMF.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
